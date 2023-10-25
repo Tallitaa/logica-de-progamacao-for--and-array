7d563d6463bcb8c17813ce6b7993a1bd683cb511
tallitaa <tallitamoreiras2@gmail.com>
2023-10-25 14:57:23 -0300

    teste

--- ex12.js
+++ ex12.js
@@ -6,13 +6,3 @@ for (var index = 0; decimal > 1 ; index++ ) {
     var resto = decimal % 2
     decimal = Math.floor(decimal / 2)
 
-    bin = bin + resto
-}
-
-var result = ''
-
-for (index = bin.length - 1; index >= 0; index--) {
-    result += bin[index]
-}
-
-console.log(1 + result)
